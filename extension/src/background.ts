4fa277d7ebc27b4800e97f8afe2ee9743b198f05
eliad1998 <eliad98@gmail.com>
2025-11-12 01:39:08 +0200

    Monitoring hardcoded address works

---
+++ extension/src/background.ts
@@ -0,0 +1,22 @@
+export { }
+
+declare const browser: typeof chrome;
+
+console.log("Background script running");
+
+const browserApi = (typeof chrome !== 'undefined' && chrome.runtime)
+  ? chrome
+  : (typeof browser !== 'undefined' ? browser : undefined);
+
+if (!browserApi || !browserApi.webRequest) {
+  console.error("Web Request API not found! Ensure script runs in Background context and permissions are set.");
+}
+
+else {
+  browserApi.webRequest.onCompleted.addListener(
+    (details) => {
+      console.log("HTTP request detected:", details.url);
+    },
+    { urls: ["*://*.facebook.com/*"] }
+  );
+}
--- extension/src/background.tsx
+++
@@ -1,8 +0,0 @@
-export {}
- 
-console.log(
-  "Live now; make now always the most precious time. Now will never come again."
-)
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Background worker loaded!")
-});
